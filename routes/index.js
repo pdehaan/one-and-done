d6f90c2a6968aabbf346aae84036dfe9c5cb74ec
Edwin Wong <edwong@mozilla.com>
2013-10-15 09:50:19 -0700

    add take task raw

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,59 @@
  * GET home page.
  */
 
-var fs = require("fs"),
+var Firebase = require('firebase'),
+    fs = require("fs"),
     path = require("path"),
-    request = require('request');
+    request = require('request'),
+    url = require("url");
 
+var dbBaseUrl = "https://onedone-dev.firebaseIO.com";
+
+function getTasks(){
+
+}
 
 exports.index = function (req, res) {
   "use strict";
+  var q = url.parse(req.url, true).query;
+  var user = q.user || "";
+  var task_id = q.task_id || "";
 
-  request('https://onedone-dev.firebaseIO.com/.json', function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      console.log(body);
-      res.render("index", {
-        "title": "Mozilla One and Done",
-        "tasks": JSON.parse(body)
+  //fetch all tasks
+  var tasks = new Firebase(dbBaseUrl+'/tasks');
+  tasks.once('value', function(dataSnapshot) {
+    var taskData = dataSnapshot.val();
+
+    // Take a task
+    if (q.user && q.task_id){
+      var users = new Firebase(dbBaseUrl+'/users');
+      users.child(q.user).once('value', function(userData) {
+        var newTotalTasks = userData.val().totalTasks+1;
+        users.child(q.user).set({currentTask:q.task_id, totalTasks:newTotalTasks});
+
+        res.render("index", {
+          "title": "Mozilla One and Done",
+          "tasks": taskData,
+          "user": user,
+          "task_id": task_id
+        });
       });
+    }else{
+        // TODO: big hack due to async issue
+        res.render("index", {
+          "title": "Mozilla One and Done",
+          "tasks": taskData,
+          "user": user,
+          "task_id": task_id
+        });
     }
+
   });
 };
+
+
+exports.leaderboard = function (req, res) {
+  "use strict";
+
+};
+
