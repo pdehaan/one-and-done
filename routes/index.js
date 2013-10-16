633ebcc0434ab189164979ca4f9fd703290b4fa8
Peter deHaan <pdehaan@mozilla.com>
2013-10-16 13:01:59 -0700

    updating routes and trying to integrate Persona

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 var Firebase = require('firebase'),
-    url = require("url");
+    url = require("url"),
+    verify = require('browserid-verify');
 
 var dbBaseUrl = "https://onedone-dev.firebaseIO.com";
 
@@ -94,4 +95,39 @@ exports.leaderboard = function (req, res) {
     });
   });
 };
+
+
+exports.auth = function (audience) {
+  return function (req, resp) {
+    console.info('verifying with persona');
+
+    var assertion = req.body.assertion;
+
+    verify(assertion, audience, function (err, email, data) {
+      if (err) {
+        // return JSON with a 500 saying something went wrong
+        console.warn('request to verifier failed : ' + err);
+        return resp.send(500, { status : 'failure', reason : '' + err });
+      }
+
+      // got a result, check if it was okay or not
+      if (email) {
+        console.info('browserid auth successful, setting req.session.email');
+        req.session.email = email;
+        return resp.redirect('/');
+      }
+
+      // request worked, but verfication didn't, return JSON
+      console.error(data.reason);
+      resp.send(403, data);
+    });
+  };
+
+};
+
+exports.logout = function (req, resp) {
+  req.session.destroy();
+  resp.redirect('/');
+};
+
 
--- server.js
+++ server.js
@@ -10,15 +10,21 @@ var path = require('path');
 var express = require('express');
 var routes = require('./routes');
 
+var PORT = process.env.PORT || 3000;
+var AUDIENCE = 'http://localhost:' + PORT;
+
 var app = express();
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', PORT);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({'secret': 'What Does the Fox Say'}));
+app.use(express.csrf());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,6 +36,8 @@ if (app.get('env') === 'development') {
 app.get('/', routes.index);
 app.get('/tasks', routes.tasks);
 app.get('/leaderboard', routes.leaderboard);
+app.get('/logout', routes.logout);
+app.post('/auth', routes.auth(AUDIENCE));
 
 http.createServer(app).listen(app.get('port'), function () {
   "use strict";
