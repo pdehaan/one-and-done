49d2ebcda97f297622f8f187bb4b06c115386be2
David Clarke <onecyrenus@gmail.com>
2013-10-23 15:22:53 -0700

    view task capability, also make sure you can't click done on a task multiple times.

--- routes/tasks.js
+++ routes/tasks.js
@@ -12,14 +12,17 @@ exports.tasks = function (req, res) {
   //Get All User Tasks
   if(req.session.auth) {
     var user_id = req.session.auth.id;
-    var task_id = parseInt(req.params.task_id, 10) || 0;
-    //fetch all tasks
-    fb.child('tasks').once('value', function (tasks) {
+    var task_id = parseInt(req.params.task_id, 10);
+    // Get user info
+    fb.child("users/" + user_id).once('value', function (userData) {
+      console.log(userData.val());
+      //fetch all tasks
+      fb.child('tasks').once('value', function (tasks) {
       //Get user's current task
-      fb.child('tasks/' + task_id).once('value', function (userTask) {
-        // Get user info
-        console.log(userTask.val());
-        fb.child("users/" + user_id).once('value', function (userData) {
+      var task_id = userData.val().currentTaskId || -1;
+        fb.child('tasks/' + task_id).once('value', function (userTask) {
+          // Get user info
+
           res.render("tasks", {
             "userData": userData.val(),
             "currentTask": userTask.val(),
@@ -40,43 +43,49 @@ exports.tasks = function (req, res) {
   * GET Assign task to user
   */
 
- exports.view = function (req, res) {
-    
- }
+exports.view = function (req, res) {
+  var fb = new Firebase(DB_BASE_URL + '/tasks/'); 
+  var task_id = parseInt(req.params.task_id, 10) || -1;
+  fb.child(task_id).once('value', function (task) {
+    res.render("viewtask", {
+      "task": task.val()
+     });
+  });
+}
 
- exports.take = function (req, res) {
-   "use strict";
-   if (req.session.auth && parseInt(req.params.task_id) > 0) {
-     var user_id = req.session.auth.id;
-     var task_id = parseInt(req.params.task_id, 10) || 0;
-     var fb = new Firebase(DB_BASE_URL);
- 
-     // User takes task, update db
-     var epoch = Math.round(Date.now() / 1000);
-     console.log('epoch: ' + epoch);
-     // Add new user with data
-     fb.child("users/" + user_id).once('value', function (userData) {
-       console.log(userData.val());
-       // Update current task info for user
-       fb.child("users/" + user_id).update({
-         "currentTaskId": task_id,
-         "currentTaskComplete": 0,
-         "currentTaskClaimedDate": epoch
-       });
-       // Update task order to bottom of stack
-       fb.child("tasks/" + task_id).update({
-         "order": 1
-       });
-       res.redirect('/tasks');
-     });
-   } else {
-     res.json({
-       "status": "fail",
-       "user_id": user_id,
-       "task_id": task_id
-     });
-   }
- };
+exports.take = function (req, res) {
+  "use strict";
+  if (req.session.auth && parseInt(req.params.task_id) > 0) {
+   var user_id = req.session.auth.id;
+    var task_id = parseInt(req.params.task_id, 10) || 0;
+    var fb = new Firebase(DB_BASE_URL);
+
+    // User takes task, update db
+    var epoch = Math.round(Date.now() / 1000);
+    console.log('epoch: ' + epoch);
+    // Add new user with data
+    fb.child("users/" + user_id).once('value', function (userData) {
+      console.log(userData.val());
+      // Update current task info for user
+     fb.child("users/" + user_id).update({
+        "currentTaskId": task_id,
+        "currentTaskComplete": 0,
+        "currentTaskClaimedDate": epoch
+      });
+      // Update task order to bottom of stack
+      fb.child("tasks/" + task_id).update({
+        "order": 1
+      });
+      res.redirect('/tasks');
+    });
+  } else {
+    res.json({
+     "status": "fail",
+      "user_id": user_id,
+      "task_id": task_id
+    });
+  }
+};
 
 /*
 * GET Cancel task
@@ -111,7 +120,7 @@ exports.cancel = function (req, res) {
    "use strict";
 
    if(req.session.auth && parseInt(req.params.task_id) > 0 ) {
-     var user_id = user.auth.id;
+     var user_id = req.session.auth.id;
      var task_id = parseInt(req.params.task_id, 10) || 0;
      var fb = new Firebase(DB_BASE_URL);
 
@@ -121,8 +130,14 @@ exports.cancel = function (req, res) {
        console.log(userData.val());
        // Get current completed_tasks
        fb.child("users/" + user_id + "/completed_tasks").once('value', function (userTasks) {
-         var completedTasksString = userTasks.val() + "," + task_id;
-         var newCompletedTasks = JSON.parse("[" + completedTasksString + "]");
+         //var completedTasksString = userTasks.val() ? userTasks.val() + "," + task_id : task_id;
+         var newCompletedTasks = [task_id];
+         var tasks = userTasks.val();
+         if(userTasks.val() && tasks.indexOf(task_id) == -1) {
+           newCompletedTasks = newCompletedTasks.concat(userTasks.val());
+           console.log('\n\n' + JSON.stringify(newCompletedTasks) + '\n\n');
+         }
+        console.log('\n\n' + "seemingly not a candidate for addition");
          // Update current task info for user
          fb.child("users/" + user_id).update({
            "currentTaskId": task_id,
--- server.js
+++ server.js
@@ -57,6 +57,7 @@ app.get('/tasks', task.tasks);
 app.get('/task/take/:task_id', task.take);
 app.get('/task/complete/:task_id', task.complete);
 app.get('/tasks/cancel/:task_id', task.cancel);
+app.get('/task/view/:task_id', task.view);
 app.get('/leaderboard', routes.leaderboard);
 app.get('/logout', auth.logout);
 app.post('/auth', auth.auth);
